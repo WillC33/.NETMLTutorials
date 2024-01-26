d12689c22684495fa24e2e2c76cb007a67891cad
WillC33 <william@williamcooke.net>
2024-01-26 16:20:46 +0000

    K means clustering algorithm

---
+++ MLConsole/KMeansClustering.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ML;
+
+namespace MLConsole;
+
+public class KMeansClustering
+{
+    string _dataPath = Path.Combine(Environment.CurrentDirectory, "TrainingData", "iris.data");
+    string _modelPath = Path.Combine(Environment.CurrentDirectory, "Outputs", "IrisClusteringModel.zip");
+    private MLContext _mlContext;
+    
+    public KMeansClustering()
+    {
+        _mlContext = new(seed: 0);
+        IDataView dataView = _mlContext.Data.LoadFromTextFile<IrisDataModel>(_dataPath, hasHeader: false, separatorChar: ',');
+        
+        string featuresColumnName = "Features";
+        var pipeline = _mlContext.Transforms
+            .Concatenate(featuresColumnName, "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
+            .Append(_mlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: 3));
+        
+        var model = pipeline.Fit(dataView);
+
+        using FileStream fileStream = new(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write);
+        _mlContext.Model.Save(model, dataView.Schema, fileStream);
+        
+        //Predicts
+        IrisDataModel data = new() //Setosa
+        {
+            SepalLength = 5.1f,
+            SepalWidth = 3.5f,
+            PetalLength = 1.4f,
+            PetalWidth = 0.2f
+        };
+        
+        var predictor = _mlContext.Model.CreatePredictionEngine<IrisDataModel, ClusterPrediction>(model);
+        var prediction = predictor.Predict(data);
+        Console.WriteLine($"Cluster: {prediction.PredictedClusterId}"); // This isn't very accurate and multiple training instances create very different and inconsistent results
+        Console.WriteLine($"Distances: {string.Join(" ", prediction.Distances ?? Array.Empty<float>())}");
+    }
+}
--- MLConsole/Program.cs
+++ MLConsole/Program.cs
@@ -2,15 +2,26 @@
 using Microsoft.ML;
 using MLConsole;
 
+// ====================================================================
+// REGRESSION MODEL
 
 //TaxiFareModel taxiFareModel = new();
-SupportIssueModel supportIssueModel = new();
-GitHubIssue singleIssue = new() { Title = "Entity Framework crashes", Description = "When connecting to the database, EF is crashing" };
-GitHubIssue singleIssue2 = new() { Title = "Logging isn't working", Description = "The logs in my output are all blank" };
-GitHubIssue singleIssue3 = new() { Title = "My cookies aren't storing in my browser so the responses are all 401", Description = "When I log in I am redirected to the login screen over and over again. I've looked at the cookies that are being stored and they do not ever appear in my localstorage" };
 
-supportIssueModel.PredictIssue(singleIssue);
-supportIssueModel.PredictIssue(singleIssue2);
-supportIssueModel.PredictIssue(singleIssue3);
+// ====================================================================
+// MULTI CATEGORY CLASSIFIER
+
+//SupportIssueModel supportIssueModel = new();
+//GitHubIssue singleIssue = new() { Title = "Entity Framework crashes", Description = "When connecting to the database, EF is crashing" };
+//GitHubIssue singleIssue2 = new() { Title = "Logging isn't working", Description = "The logs in my output are all blank" };
+//GitHubIssue singleIssue3 = new() { Title = "My cookies aren't storing in my browser so the responses are all 401", Description = "When I log in I am redirected to the login screen over and over again. I've looked at the cookies that are being stored and they do not ever appear in my localstorage" };
+
+//supportIssueModel.PredictIssue(singleIssue);
+//supportIssueModel.PredictIssue(singleIssue2);
+//supportIssueModel.PredictIssue(singleIssue3);
+
+for(int i = 0; i <= 50; i++)
+{
+    KMeansClustering iris = new();
+}
 
 Console.WriteLine("Exit");
